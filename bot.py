29e213d44d92fe6296106bb0d47e5278894ad0c9
v5nkyq4xps-beep <v5nkyq4xps@privaterelay.appleid.com>
2026-02-02 20:59:36 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,6 +8,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Bot attivo")
 
 app = ApplicationBuilder().token(TOKEN).build()
+
 app.add_handler(CommandHandler("start", start))
 
 app.run_polling()
